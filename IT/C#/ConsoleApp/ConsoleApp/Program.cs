69608a40e9f0c274fe1fffebec54cdca0328da3c
Halász Noel <halasz.noel.2021e@ms.gdszeged.hu>
2024-09-11 13:33:29 +0200

    9/11

---
+++ IT/C#/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //kampitas.hu letöltések/programozási anyagok/programozasialapok.pps változó típusok, és operátorok (dolgozatban)
+            //A változókat deklarálni kell (meg kell mondani, hogy a változónak mi a típusa)
+            string duma = "rawr..."; //a duma változó string típusú lesz
+            string lala; //a lala változó string típusú, de az értékét még nem adtam meg
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(duma);
+            byte magasság = 255;
+            magasság++;
+            Console.WriteLine(magasság);
+            /*0 lesz a magasság, mert a byte típus maximum 255-ig tárolhat. 255 + 1 az ebben az esetben 0 lesz, mert túlcsordul a  változó*/
+
+            int number = 100;
+            Console.WriteLine(++number); //előbb növel
+            Console.WriteLine(number);
+            Console.WriteLine("------------------------------------");
+            int number2 = 100;
+            Console.WriteLine(number2++); //előbb az eredeti értéket adja eredményül aztán növel
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(number2);
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IT/C#/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //szöveg beolvasása billentyűzetről és eltárolása egy változóban.
+            Console.Write("Kérem adja meg a nevét: ");
+            string name = Console.ReadLine(); //A ReadLine olvassa be a szöveget
+            Console.WriteLine($"Szia {name}!"); //A $-jel hatására a {} között levő kifejezés értéke fog kiíródni
+
+            //int típusú érték beolvasása
+            Console.Write("Kérem adja meg a fizetését: ");
+            int fizu = int.Parse(Console.ReadLine()); //int.Parse(szöveg) a szöveget int típusúvá alakítja
+            //van long.Parse, byte.Parse stb. is
+
+            if (fizu < 350000)
+                Console.WriteLine("Keress új munkahelyet!");
+            else
+                Console.WriteLine("Nem rossz, de azért még lehet fejlődni.");
+            
+
+
+            Console.ReadKey(); //Vár egy billentyű leütésre
+        }
+    }
+}
