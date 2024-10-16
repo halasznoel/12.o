752983bdc5c427b8803fbd59a0de24cf78397622
Halász Noel <halasz.noel.2021e@ms.gdszeged.hu>
2024-10-16 13:35:46 +0200

    mentes

---
+++ IT/C#/2024-10-16/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nevek = new List<string>();
+            for (int db = 0; db < 5; db++)
+            {
+                Console.Write("Írj egy nevet: ");
+                nevek.Add(Console.ReadLine());
+            }
+            int i = 0;
+            foreach (string nev in nevek)
+            {
+                i++;
+                Console.WriteLine($"{i}. {nev}");
+            }
+            Console.ReadLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IT/C#/2024-10-16/struktgyak/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struktgyak
+{
+    class Program
+    {
+        /*Struktúra használata: összetartozó adatok esetén*/
+
+        struct Adat
+        {
+            //definiáljuk, hogy milyen mezői vannak a struktúrának (field)
+            public string név;
+            public string város;
+            public int fizetés;
+        }
+
+        static void Main(string[] args)
+        {
+            Adat pista,Feri;
+            pista.név = "Savanya István";
+            pista.város = "Szeged";
+            pista.fizetés = 500000;
+            Feri.név = "Fecó";
+            Feri.város = "Törökszentmiklós";
+            Feri.fizetés = 1200000;
+
+            Console.WriteLine(pista.név);
+            Console.WriteLine(Feri.fizetés);
+
+            /*Olyan lista, ami Adat struktúrát tartalmaz */
+            List<Adat> adatok = new List<Adat>();
+            Adat temp;
+            temp.név = "Madaras Fecó";
+            temp.város = "Butapest";
+            temp.fizetés = 3600000;
+            adatok.Add(temp);
+            temp.név = "Palik";
+            temp.város = "Kecskemét";
+            temp.fizetés = 1200000;
+            adatok.Add(temp);
+
+            /*Jelenítsük meg az adatok lista tartalmát*/
+            foreach (Adat item in adatok)
+            {
+                Console.WriteLine($"Név: {item.név} Város: {item.város} Fizetés: {item.fizetés}");
+            }
+
+            Console.ReadKey();
+        }   
+    }
+}
