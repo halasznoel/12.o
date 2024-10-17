fa0bb23eab8aab748fd0f999f5532cb6c89a459d
Halász Noel <halasz.noel.2021e@ms.gdszeged.hu>
2024-10-17 13:39:16 +0200

    save

---
+++ IT/C#/2024-10-17/listas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listas
+{
+    class Program
+    {
+        struct Adat
+        {
+            public string rendszám;
+            public string típus;
+            public string szín;
+        }
+        static void Main(string[] args)
+        {
+            List<Adat> adatok = new List<Adat>();
+            for (int i = 0; i < 4; i++)
+            {
+                Adat auto;
+                Console.Write("Add meg az autó rendszámát: ");
+                auto.rendszám = Console.ReadLine();
+                Console.Write("Add meg az autó típusát: ");
+                auto.típus = Console.ReadLine();
+                Console.Write("Add meg az autó színét: ");
+                auto.szín = Console.ReadLine();
+                adatok.Add(auto);
+
+            }
+            foreach (Adat item in adatok)
+            {
+                Console.WriteLine($"Rendszám: {item.rendszám} Típus: {item.típus} Szín: {item.szín}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
