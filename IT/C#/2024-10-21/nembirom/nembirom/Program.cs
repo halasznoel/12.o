16dbe94a8bbbf3603d10288161411b3da57525c7
Halász Noel <halasz.noel.2021e@ms.gdszeged.hu>
2024-10-21 13:15:07 +0200

    save

---
+++ IT/C#/2024-10-21/nembirom/nembirom/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace szovegfajlbairas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            /* 2020 óta List<int> numbers2 = new() */
+
+            string nevek = "Horvát-Fock Erik Tamás, Juhász Lajos Csaba, Terbán Kamilla";
+            //szeretnénk szétválasztani a ,-vel tagolt elemeket
+            string[] names = nevek.Split(','); //a split parancs egy tömböt ad eredményül Split('határolójel');
+
+            //Írjuk ki a neveket egy szövegfájlba
+            StreamWriter fp = new StreamWriter("nevek.txt",false,Encoding.UTF8); //false jelenti, hogy nem hozzáfűzés (append) van hanem törlődik a file tartalma
+            foreach (var item in names) //a names tömb minden elemét végigolvasom, a lista elemei egymás után az item változóba kerülnek
+            {
+                fp.WriteLine(item);
+            }
+            fp.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
