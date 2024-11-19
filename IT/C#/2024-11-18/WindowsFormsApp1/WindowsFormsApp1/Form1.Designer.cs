144f5b884c0c132c53b9248a294ec7eaa29af748
Halász Noel <halasz.noel.2021e@ms.gdszeged.hu>
2024-11-19 09:40:39 +0100

    save

---
+++ IT/C#/2024-11-18/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.a = new System.Windows.Forms.TextBox();
+            this.b = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.calculate = new System.Windows.Forms.Button();
+            this.muvelet = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // a
+            // 
+            this.a.Location = new System.Drawing.Point(33, 39);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(100, 20);
+            this.a.TabIndex = 0;
+            // 
+            // b
+            // 
+            this.b.Location = new System.Drawing.Point(266, 39);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(100, 20);
+            this.b.TabIndex = 1;
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(33, 85);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(100, 20);
+            this.result.TabIndex = 2;
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(155, 83);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(75, 23);
+            this.calculate.TabIndex = 3;
+            this.calculate.Text = "Számítás";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // muvelet
+            // 
+            this.muvelet.FormattingEnabled = true;
+            this.muvelet.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.muvelet.Location = new System.Drawing.Point(139, 38);
+            this.muvelet.Name = "muvelet";
+            this.muvelet.Size = new System.Drawing.Size(121, 21);
+            this.muvelet.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.muvelet);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.b);
+            this.Controls.Add(this.a);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Program_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox a;
+        private System.Windows.Forms.TextBox b;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.ComboBox muvelet;
+    }
+}
+
---
+++ IT/C#/2024-11-18/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Program_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Na mi van!");
+            float a_value = float.Parse(a.Text);
+            float b_value = float.Parse(b.Text);
+            int melyikmuv = muvelet.SelectedIndex;
+
+            if(melyikmuv == -1)
+                MessageBox.Show(muvelet.Items[melyikmuv].ToString());
+            else
+            {
+                string muv = muvelet.Items[melyikmuv].ToString();
+                //muv változóban * vagy / vagy - vagy + valamelyike lesz
+                switch(muv)
+                {
+                    case "*":
+                        result.Text = (a_value * b_value).ToString();
+                        break;
+                    case "/":
+                        result.Text = (a_value / b_value).ToString();
+                        break;
+                    case "+":
+                        result.Text = (a_value + b_value).ToString();
+                        break;
+                    case "-":
+                        result.Text = (a_value - b_value).ToString();
+                        break;
+                }
+            }
+        }
+    }
+}
