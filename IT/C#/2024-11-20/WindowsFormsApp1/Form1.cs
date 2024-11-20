f11ca70a6982f34b1ae211bba65a4ed05c674021
Halász Noel <halasz.noel.2021e@ms.gdszeged.hu>
2024-11-20 13:39:44 +0100

    save

---
+++ IT/C#/2024-11-20/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        List<string> towns = new List<string> { "Butapest", "Kardoskút", "Sopron", "Veszprém" };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //towns_combo.Items.Add(a város)
+            foreach (var item in towns)
+            {
+                comboBox1.Items.Add(item);
+            }
+        }
+    }
+}
