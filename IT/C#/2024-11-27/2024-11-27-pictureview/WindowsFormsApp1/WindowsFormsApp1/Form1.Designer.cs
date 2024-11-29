d2d124983924df0b80b013b0f8e889a66015cc5b
Halász Noel <halasz.noel.2021e@ms.gdszeged.hu>
2024-11-29 13:48:16 +0100

    save

---
+++ IT/C#/2024-11-27/2024-11-27-pictureview/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kép = new System.Windows.Forms.PictureBox();
+            this.képválasztó = new System.Windows.Forms.OpenFileDialog();
+            this.képválasztógomb = new System.Windows.Forms.Button();
+            this.előzmények = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.kép)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kép
+            // 
+            this.kép.Location = new System.Drawing.Point(474, 45);
+            this.kép.Name = "kép";
+            this.kép.Size = new System.Drawing.Size(547, 469);
+            this.kép.TabIndex = 0;
+            this.kép.TabStop = false;
+            // 
+            // képválasztó
+            // 
+            this.képválasztó.FileName = "openFileDialog1";
+            // 
+            // képválasztógomb
+            // 
+            this.képválasztógomb.Location = new System.Drawing.Point(493, 555);
+            this.képválasztógomb.Name = "képválasztógomb";
+            this.képválasztógomb.Size = new System.Drawing.Size(129, 23);
+            this.képválasztógomb.TabIndex = 1;
+            this.képválasztógomb.Text = "Kép kiválasztása";
+            this.képválasztógomb.UseVisualStyleBackColor = true;
+            this.képválasztógomb.Click += new System.EventHandler(this.képválasztógomb_Click);
+            // 
+            // előzmények
+            // 
+            this.előzmények.FormattingEnabled = true;
+            this.előzmények.Location = new System.Drawing.Point(26, 64);
+            this.előzmények.Name = "előzmények";
+            this.előzmények.Size = new System.Drawing.Size(423, 394);
+            this.előzmények.TabIndex = 2;
+            this.előzmények.SelectedIndexChanged += new System.EventHandler(this.előzmények_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1227, 696);
+            this.Controls.Add(this.előzmények);
+            this.Controls.Add(this.képválasztógomb);
+            this.Controls.Add(this.kép);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.kép)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox kép;
+        private System.Windows.Forms.OpenFileDialog képválasztó;
+        private System.Windows.Forms.Button képválasztógomb;
+        private System.Windows.Forms.ListBox előzmények;
+    }
+}
+
---
+++ IT/C#/2024-11-27/2024-11-27-pictureview/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void képválasztógomb_Click(object sender, EventArgs e)
+        {
+            képválasztó.ShowDialog();
+            kép.Load(képválasztó.FileName);
+            előzmények.Items.Add(képválasztó.FileName);
+        }
+
+        private void előzmények_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            kép.Load(előzmények.SelectedItem.ToString());
+        }
+    }
+}
