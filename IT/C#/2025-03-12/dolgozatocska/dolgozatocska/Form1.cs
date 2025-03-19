f19232510fb0c410313a8e99aa7c9f5380347d0c
Halász Noel <halasz.noel.2021e@ms.gdszeged.hu>
2025-03-19 13:32:51 +0100

    save

---
+++ IT/C#/2025-03-12/dolgozatocska/dolgozatocska/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace dolgozatocska
+{
+    public partial class Form1 : Form
+    {
+        List<int> szamok = new List<int>();
+        List<string> szoveg = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            string[] olvasas = File.ReadAllLines("adatok.dat"); //File beolvasás
+            foreach (var item in olvasas) //Végig olvassuk a fájlt tartalmát a foreachel
+            {
+                string[] elemek = item.Split(' '); //Szétválasztjuk
+                szamok.Add(int.Parse(elemek[0])); //A szám adatok.datból
+                szoveg.Add(elemek[1]); //A számhoz tartozó szöveg
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            int szam = int.Parse(textBox1.Text);
+
+            try
+            {
+                for (int i = 0; i < szoveg.Count(); i++) //számozást állítunk fel
+                {
+                    if (szamok[i] == szam)
+                    {
+                        listBox1.Items.Add(szoveg[i]);
+                    }
+                }
+            }
+            catch { }
+        }
+    }
+}
---
+++ IT/C#/2025-03-19-treeview/treeview/treeview/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace treeview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tv_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tv.Nodes.Add("növények");
+
+            tv.Nodes[0].Nodes.Add("nyitvatermők");
+            tv.Nodes[0].Nodes[0].Nodes.Add("fenyő");
+            tv.Nodes[0].Nodes[0].Nodes[0].Nodes.Add("erdei fenyő");
+            tv.Nodes[0].Nodes[0].Nodes[0].Nodes.Add("lucfenyő");
+
+            tv.Nodes[0].Nodes.Add("zárvatermők");
+            tv.Nodes[0].Nodes[1].Nodes.Add("orgona");
+            tv.Nodes[0].Nodes[1].Nodes.Add("barack");
+            tv.Nodes[0].Nodes[1].Nodes.Add("sóska");
+
+
+
+
+            tv.Nodes.Add("állatok");
+
+            tv.Nodes[1].Nodes.Add("emlősök");
+            tv.Nodes[1].Nodes[0].Nodes.Add("macska");
+            tv.Nodes[1].Nodes[0].Nodes.Add("kutya");
+            tv.Nodes[1].Nodes[0].Nodes.Add("róka");
+
+            tv.Nodes[1].Nodes.Add("madarak");
+            tv.Nodes[1].Nodes[1].Nodes.Add("tyúk");
+            tv.Nodes[1].Nodes[1].Nodes.Add("liba");
+            tv.Nodes[1].Nodes[1].Nodes[1].Nodes.Add("okos liba");
+            tv.Nodes[1].Nodes[1].Nodes[1].Nodes.Add("buta liba");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tv.SelectedNode.Remove();
+        }
+    }
+}
